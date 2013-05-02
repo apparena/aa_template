169ce79974b5b131c1297d69da6a01c408115009
gpollock <guntrampollock@googlemail.com>
2013-05-02 18:45:51 +0200

    add config

--- config.php
+++ config.php
@@ -20,4 +20,14 @@ $db_pass = "template";
 $fb_app_id = "161596157315481";
 $fb_app_secret = "cb11ea19d870420d6df37795cec44606";
 */
+
+/* auth module config */
+// client id for web applications, needed by the sign in button
+$gplus_client_id = "990596349199.apps.googleusercontent.com";
+
+$twitter_consumer_key = "2WnBPqfOf0vaGPJsMFG6fw";
+$twitter_consumer_secret = "mygq0bS2LQfUn6jLiujZ8VWOqXCBKtxHtXgnGnrMc";
+// this url will be called after the user accepts or declines the login
+$twitter_callback_url = "https://www.app-arena.com/app/aa_template/dev/modules/auth/twitter_auth_callback.php";
+https://www.app-arena.com/app/aa_template/dev/
 ?>
