9d97e8f5b77161d790733b95ea929bd3be905bc2
gpollock <guntrampollock@googlemail.com>
2013-05-02 19:00:12 +0200

    add social demo template improve auth init

--- js/main.js
+++ js/main.js
@@ -68,6 +68,8 @@ require([
         aa_tmpl_load('terms.phtml');
     });
     
+    aa.auth = auth;
+    
     auth.init({
 		placement: {
 			// "template[0]" will be mapped to "toElement[0]" and so on...
