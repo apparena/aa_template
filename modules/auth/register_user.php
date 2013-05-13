d5a5a07db856ab17dd9931715dcec90fc7c543db
gpollock <guntrampollock@googlemail.com>
2013-05-13 18:00:18 +0200

    get_client_ip seems already to be accessible because of functions include in init

--- modules/auth/register_user.php
+++ modules/auth/register_user.php
@@ -99,17 +99,4 @@
 	echo json_encode( array( 'success' => 'yay!' ) );
 	exit( 0 );
 	
-	function get_client_ip()
-	{
-		// Get client ip address
-		if (isset($_SERVER["REMOTE_ADDR"]))
-			$client_ip = $_SERVER["REMOTE_ADDR"];
-		else if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
-			$client_ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-		else if (isset($_SERVER["HTTP_CLIENT_IP"]))
-			$client_ip = $_SERVER["HTTP_CLIENT_IP"];
-		
-		return $client_ip;
-	}
-	
 ?>
