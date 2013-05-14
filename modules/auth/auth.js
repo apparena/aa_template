5bae1fafc2d0b57a646cf09ac5d5cc0e5a5b0492
gpollock <guntrampollock@googlemail.com>
2013-05-14 14:32:00 +0200

    fix logic

--- modules/auth/auth.js
+++ modules/auth/auth.js
@@ -81,11 +81,11 @@ define(
 			 * Overwrite settings if set and
 			 * initialize the ui elements.
 			 * @param {Object} newSettings The settings which will override the default settings.
-			 * @param {Boolean} doNotCheckStatus If the init function shall check for the user's
-			 * 									 loginstatus and show the profile if the user is
-			 * 									 logged in.
+			 * @param {Boolean} checkStatus If the init function shall check for the user's
+			 * 								loginstatus and show the profile if the user is
+			 * 								logged in.
 			 */
-			init: function ( newSettings, doNotCheckStatus ) {
+			init: function ( newSettings, checkStatus ) {
 				
 				// just copy the function scope for use in child functions here in the init (which have different "this"-scopes!)
 				var that = this;
@@ -254,7 +254,7 @@ define(
 				// just to make sure FB has parsed stuff... (i know it wont if we dont call this ;) )
 				FB.XFBML.parse();
 				
-				if ( doNotCheckStatus == false ) {
+				if ( checkStatus == true ) {
 					
 					this.checkStatus();
 					
